427de7d9f2b7d70d2e875252053bef0012c375fc
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-28 19:13:27 -0500

    added react strap and inputs

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
 
 class MadlibForm extends Component {
+
+    handleChange = function () {
+
+    }
+    
     render() {
         return (
-            <div>
-                <h1>Bottega Mad Libs Form Component</h1>
-            </div>
+            <Row style={{textAlign: 'center', color: 'white'}}>
+
+                <Col md='3' className="inputWrapper">
+                    <Row>
+                        <Col md='2'>
+                            <label className="greenLabel">1</label>
+                        </Col>
+
+                        <Col md="10">
+                            <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col md='3' className="inputWrapper">
+                    <Row>
+                        <Col md='2'>
+                            <label className="greenLabel">1</label>
+                        </Col>
+
+                        <Col md="10">
+                            <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col md='3' className="inputWrapper">
+                    <Row>
+                        <Col md='2'>
+                            <label className="greenLabel">1</label>
+                        </Col>
+
+                        <Col md="10">
+                            <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        </Col>
+                    </Row>
+                </Col>
+
+                <Col md='3' className="inputWrapper">
+                    <Row>
+                        <Col md='2'>
+                            <label className="greenLabel">1</label>
+                        </Col>
+
+                        <Col md="10">
+                            <input placeholder="Color" type="text" onChange={this.handleChange} />
+                        </Col>
+                    </Row>
+                </Col>
+                
+            </Row>
         );
     };
 }
